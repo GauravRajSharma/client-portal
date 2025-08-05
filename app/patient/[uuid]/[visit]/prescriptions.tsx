4568e92f993c6637620c571c2749ef96b4a83f18
GauravRajSharma <gasharma@who.int>
2025-08-05 15:37:25 +0545

    init

--- app/patient/[uuid]/[visit]/prescriptions.tsx
+++ app/patient/[uuid]/[visit]/prescriptions.tsx
@@ -7,64 +7,64 @@ import { trpc } from "@/utils/trpc";
 import { Platform } from "react-native";
 
 interface HTMLContentViewerProps {
-  html: string;
-  style?: React.CSSProperties | any; // Support both web and RN styles
-  className?: string;
+	html: string;
+	style?: React.CSSProperties | any; // Support both web and RN styles
+	className?: string;
 }
 
 const HTMLContentViewer: React.FC<HTMLContentViewerProps> = ({
-  html,
-  style,
-  className,
+	html,
+	style,
+	className,
 }) => {
-  // For React Native
-  if (Platform.OS !== "web") {
-    const { WebView } = require("react-native-webview");
-    return <WebView originWhitelist={["*"]} source={{ html }} style={style} />;
-  }
+	// For React Native
+	if (Platform.OS !== "web") {
+		const { WebView } = require("react-native-webview");
+		return <WebView originWhitelist={["*"]} source={{ html }} style={style} />;
+	}
 
-  // For Web
-  return (
-    <iframe
-      srcDoc={html}
-      style={{
-        border: "none",
-        width: "100%",
-        height: "100%",
-        ...style,
-      }}
-      className={className}
-      title="HTML Content"
-    />
-  );
+	// For Web
+	return (
+		<iframe
+			srcDoc={html}
+			style={{
+				border: "none",
+				width: "100%",
+				height: "100%",
+				...style,
+			}}
+			className={className}
+			title="HTML Content"
+		/>
+	);
 };
 
 export default function Prescriptions() {
-  const { uuid, visit } = useLocalSearchParams<{
-    uuid: string;
-    visit: string;
-  }>();
+	const { uuid, visit } = useLocalSearchParams<{
+		uuid: string;
+		visit: string;
+	}>();
 
-  const { data, isLoading } = trpc.patientPrescription.useQuery({ visit });
+	const { data, isLoading } = trpc.patientPrescription.useQuery({ visit });
 
-  return (
-    <View flex={1}>
-      <Button
-        rounded={0}
-        onPress={async () => {
-          router.back();
-        }}
-      >
-        <ArrowLeft /> Go to visits
-      </Button>
+	return (
+		<View flex={1}>
+			<Button
+				rounded={0}
+				onPress={async () => {
+					router.back();
+				}}
+			>
+				<ArrowLeft /> Go to visits
+			</Button>
 
-      {isLoading && <Spinner size="large" />}
-      {data && !isLoading && (
-        <HTMLContentViewer
-          html={data}
-          style={{ width: "100%" }} // Adjust as needed
-        />
-      )}
-    </View>
-  );
+			{isLoading && <Spinner size="large" />}
+			{data && !isLoading && (
+				<HTMLContentViewer
+					html={data}
+					style={{ width: "100%" }} // Adjust as needed
+				/>
+			)}
+		</View>
+	);
 }
